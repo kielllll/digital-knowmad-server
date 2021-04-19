ef79cb85c34ac81ede3e7bd57c69516121baa56b
kielllll <eatarranza@gmail.com>
2021-04-19 19:09:59 +0800

    add: Establish DB connection to Atlas

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 // Dependencies
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
 // express instance
 const app = express();
 
 // Environment variables
+const connectionString = process.env.DB_CONNECTION_STRING;
+const port = process.env.PORT || 3000;
 
 // Misc setup
-app.use(express.json()); // handle JSON objects from requests
+// handle JSON objects from requests
+app.use(express.json());
 app.use(cors());
+
+// Establish a connection to MongoDB Atlas
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+};
+
+mongoose.connect(connectionString, options);
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "connection error: ")
+);
+mongoose.connection.once("open", () =>
+  console.log("Database connection established")
+);
 
 // Routes
 // root
@@ -18,8 +40,6 @@ app.get("/", (req, res) => {
     message: "The Course Booking System API",
   });
 });
-
-const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
   console.log(`Application server is running at http://localhost:${port}`)

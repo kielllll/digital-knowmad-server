e6da894787ee9f2238f0ee826a3c2b63b1775371
kielllll <eatarranza@gmail.com>
2021-04-28 18:27:19 +0800

    add: Create the course schema and files for routes, models, and controllers

---
+++ models/Course.js
@@ -0,0 +1,28 @@
+// Course model
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+  createdOn: {
+    type: Date,
+    default: new Date(),
+  },
+  enrollees: [
+    {
+      userId: String,
+      enrolledOn: {
+        type: Date,
+        default: new Date(),
+      },
+      status: String,
+    },
+  ],
+});
+
+module.exports = mongoose.model("course", courseSchema);

859c0f4d2296d929c5862b6905e482e20b29c258
kielllll <eatarranza@gmail.com>
2021-04-29 20:05:59 +0800

    update: Retrieve a specific course details via ID in GET /api/courses?id=<course_id>

--- controllers/course.js
+++ controllers/course.js
@@ -4,3 +4,8 @@ const Course = require("../models/Course");
 module.exports.getAll = () => {
   return Course.find({}).then((courses) => courses);
 };
+
+// Retrieve a specific course
+module.exports.getCourseDetails = (id) => {
+  return Course.findById(id).then((course) => course);
+};
--- routes/courses.js
+++ routes/courses.js
@@ -7,9 +7,13 @@ const CourseController = require("../controllers/course");
 
 // Main Route
 router.get("/", (req, res) => {
-  CourseController.getAll().then((courses) => {
-    res.send({ courses });
-  });
+  if (req.query.id) {
+    CourseController.getCourseDetails(req.query.id).then((course) =>
+      res.send({ course })
+    );
+  } else {
+    CourseController.getAll().then((courses) => res.send({ courses }));
+  }
 });
 
 module.exports = router;
